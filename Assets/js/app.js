fb5379e836e6f17ec1c75e2b8e6e2e5b676b2a48
Elijah Romer <romere88@gmail.com>
2021-07-09 17:42:11 -0500

    added functionality for the time boxes to automatically render on page load from 8:00 to 18:00. Adjusted styling for better user-friendliness and appearance.

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -9,19 +9,31 @@ let updateDateTime = () => {
 
 let loadHeaderTimer = () => {
   console.log (`loadHeaderTimer FIRED`)
-  console.log(dateTimeEl);
   dateTimeEl[0].textContent =(moment().format('MMMM Do YYYY, h:mm:ss a'));
   setInterval(updateDateTime, 1000);
 }
 
+let loadTimeBlocks = () => {
+  console.log(`loadTimeBlocks FIRED`)
+  let businessOpen = 8;
+  let businessClose = 18;
+
+  for (let i = businessOpen -1; i < businessClose; i++){
+
+    let currentRowEl = $(`<div class='row'></div>`);
+      let currentHourEl = $(`<div class='col-1 hour'>${i + 1}:00</div>`);
+        currentHourEl.appendTo(currentRowEl);
+      let textAreaInput = $(`<textarea class="col-10 present"></textarea>`)
+        textAreaInput.appendTo(currentRowEl);
+      let saveButtonEl = $(`<div class="saveBtn col-1"><i class="fas fa-save"></i></div>`)
+        saveButtonEl.appendTo(currentRowEl);
+    currentRowEl.appendTo('#timeRows');
+  }
+}
 $(`document`).ready(loadHeaderTimer);
+$(`document`).ready(loadTimeBlocks);
 //END LOAD HEADER TIMER
 
-let testObject = {
-  myName: 'Elijah',
-  myFavFood: 'Sushi',
-  myFavActivity: 'Skydiving' 
-}
 
 function logProperties(testObject){
     for (let property in testObject){
@@ -29,4 +41,23 @@ function logProperties(testObject){
     }
 };
 
-logProperties(testObject);
+// GIVEN I am using a daily planner to create a schedule*************************
+
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+// DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// WHEN I scroll down
+// THEN I am presented with timeblocks for standard business hours
+
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+
+// WHEN I click into a timeblock
+// THEN I can enter an event
+
+// WHEN I click the save button for that timeblock
+// THEN the text for that event is saved in local storage
+
+// WHEN I refresh the page
+// THEN the saved events persist
