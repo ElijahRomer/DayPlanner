d8ea219d1e2e468d957b6a6db3d15b60b20b7738
Elijah Romer <romere88@gmail.com>
2021-07-12 14:46:38 -0500

    added functionality for entries to persist to local storage and automatically load in a new session.

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -17,8 +17,8 @@ let loadHeaderTimer = () => {
 
 let isPastPresentOrFuture = (timeBlockHour) => {
   //compare hour of current moment to the hour of the specified timeblock
-  // let currentHour = moment().hour();
-  let currentHour = 12
+  let currentHour = moment().hour();
+  // let currentHour = 12
   if (currentHour === timeBlockHour){
       return 'present';
   } else if(currentHour < timeBlockHour) {
@@ -38,12 +38,13 @@ let loadTimeBlocks = () => {
     let currentRowEl = $(`<div class='row'></div>`);
       let currentHourEl = $(`<div class='col-2 col-md-1 hour'>${i + 1}:00</div>`);
         currentHourEl.appendTo(currentRowEl);
-      let textAreaInput = $(`<textarea class="col-8 col-md-10 ${isPastPresentOrFuture(i + 1)}"></textarea>`)
+      let textAreaInput = $(`<textarea class="col-8 col-md-10 ${isPastPresentOrFuture(i + 1)}" id = "${i + 1}"></textarea>`)
         textAreaInput.appendTo(currentRowEl);
-      let saveButtonEl = $(`<div class="saveBtn col-2 col-md-1"><i class="fas fa-save"></i></div>`)
-        saveButtonEl.appendTo(currentRowEl);
-    currentRowEl.appendTo('#time-rows-container');
-  }
+      let saveButtonEl = $(`<div class="saveBtn col-2 col-md-1"><i class="fas fa-save"></i></div>`);
+      saveButtonEl.appendTo(currentRowEl);
+      currentRowEl.appendTo('#time-rows-container');
+    }
+    $(`i`).on('click', persistEntryToLocalStorage)
 }
 
 let refreshTimeBlocks = () => {
@@ -59,7 +60,51 @@ $(`document`).ready(loadTimeBlocks);
 //ADD EVENT LISTENER TO REFRESH TIME-BLOCKS BUTTON
 $(`#refresh`).on('click', refreshTimeBlocks);
   
+//PERSIST ENTRY TO LOCAL STORAGE 
+let persistEntryToLocalStorage = (eventObject) => {
+  console.log(`persistEntryToLocalStorage FIRED`);
+  let entryId = $(eventObject.target).parent().siblings('textarea').attr('id');
+  entryId = parseInt(entryId) - 8; //convert time to corresponding index in HTML collection
 
+  let entry = $(eventObject.target).parent().siblings('textarea').val();
+  localStorage.setItem(entryId, entry);
+
+
+  console.log(entry);
+  console.log(typeof entry);
+  console.log(entryId);
+  console.log(typeof entryId);
+}
+
+let retrieveEntriesFromLocalStorage = () => {
+  console.log(`retrieveEntriesFromLocalStorage FIRED`)
+  let textAreaHtmlCollection = $('textarea');
+  console.log(textAreaHtmlCollection);
+  console.log(`The textAreaHtmlCollection length is ${textAreaHtmlCollection.length}`);
+
+
+  for (let entryKey = 0; entryKey < textAreaHtmlCollection.length; entryKey++) {
+    if(!localStorage.getItem(entryKey)){
+      console.log(`LocalStorage has no entry for the hour ${entryKey +8}, which is entry key ${entryKey}.`)
+      continue;
+    }
+    console.log(`LocalStorage DOES HAVE an entry for the hour ${entryKey +8}, which is entry key ${entryKey}.`)
+
+    let currentEntryValue = localStorage.getItem(entryKey); // value
+    let currentEntryTextAreaEl = $(textAreaHtmlCollection[entryKey]);
+
+      currentEntryTextAreaEl.val(currentEntryValue);
+
+    console.log(currentEntryTextAreaEl)
+
+    console.log(`The entryKey for hour ${entryKey +8}:00 is ${entryKey}.`)
+    console.log(`The currentEntryValue for hour ${entryKey +8}:00 is "${currentEntryValue}".`)
+
+  }
+
+}
+
+$('document').ready(retrieveEntriesFromLocalStorage)
 
 
 
